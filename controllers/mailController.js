871b55e4a8c949dbd28a034fd5938506553a1466
kolibee <juanpjuarez@proton.me>
2025-07-05 18:21:41 -0600

    redirect to home despues de enviar mensaje

--- controllers/mailController.js
+++ controllers/mailController.js
@@ -1,6 +1,15 @@
-const Mail = require('../models/Mail');
+const Mail = require('../models/Mail')
 
 exports.enviarMail = function (req, res) {
-  let mail = new Mail(req.body);
-  mail.enviar();
+  let mail = new Mail(req.body)
+  mail
+		.enviar()
+	  .then(() => {
+			//console.log('Gracias por enviarnos su mensaje'),
+			res.redirect("/")
+		})
+	  .catch((e) => {
+			//console.log(e)
+      res.redirect("/")
+		})
 }
--- models/Mail.js
+++ models/Mail.js
@@ -1,16 +1,16 @@
-require('dotenv').config();
-const nodemailer = require("nodemailer");
-const validator = require("validator");
+require('dotenv').config()
+const nodemailer = require("nodemailer")
+const validator = require("validator")
 
 let Mail = function(data) {
-  this.data = data;
-	this.errors = [];
-};
+  this.data = data
+	this.errors = []
+}
 
 Mail.prototype.cleanUp = function () {
-	if (typeof(this.data.nombre) != "string") {this.data.nombre = ""};
-	if (typeof(this.data.email) != "string") {this.data.email = ""};
-	if (typeof(this.data.descripcion) != "string") {this.data.descripcion = ""};
+	if (typeof(this.data.nombre) != "string") {this.data.nombre = ""}
+	if (typeof(this.data.email) != "string") {this.data.email = ""}
+	if (typeof(this.data.descripcion) != "string") {this.data.descripcion = ""}
 	// get rid of any bogus properties
 	this.data = {
     nombre: this.data.nombre.trim(),
@@ -20,38 +20,44 @@ Mail.prototype.cleanUp = function () {
 }
 
 Mail.prototype.validate = function () {
-  if (this.data.nombre == "") {this.errors.push("Por favor introduzca su nombre completo.")};
-  if (this.data.nombre != "" && !validator.isAlphanumeric(this.data.nombre)) {this.errors.push("El nombre completo solo puede contener letras.")}
-  if (this.data.descripcion == "") {this.errors.push("Por favor, describa su necesidad o requerimiento.")};
-  if (!validator.isEmail(this.data.email)) {this.errors.push("Debe proporcionar un correo.")};
+  if (this.data.nombre == "") {this.errors.push("Por favor introduzca su nombre completo.")}
+  //if (this.data.nombre != "" && !validator.isAlphanumeric(this.data.nombre)) {
+	//  this.errors.push("El nombre completo solo puede contener letras.")
+	//}
+  if (this.data.descripcion == "") {this.errors.push("Por favor, describa su necesidad o requerimiento.")}
+  if (!validator.isEmail(this.data.email)) {this.errors.push("Debe proporcionar un correo.")}
   if (this.data.descripcion.length > 300) {this.errors.push("Su texto no debe exceder 300 letras.")}
   if (this.data.nombre.length > 50) {this.errors.push("El nombre completo no puede exceder 50 letras.")}
 }
 
 
-Mail.prototype.enviar = async function () {
-	this.cleanUp();
-  this.validate();
+Mail.prototype.enviar = function () {
+  return new Promise(async (resolve, reject) => {
+	  this.cleanUp()
+    this.validate()
+    if (!this.errors.length) {
+      const transporter = nodemailer.createTransport({
+        host: 'smtp.resend.com',
+        secure: true,
+        port: 465,
+        auth: {
+          user: 'resend',
+          pass: process.env.AKS,
+        },
+      })
 
-  const transporter = nodemailer.createTransport({
-    host: 'smtp.resend.com',
-    secure: true,
-    port: 465,
-    auth: {
-      user: 'resend',
-      pass: process.env.AKS,
-    },
-  });
-
-  const info = await transporter.sendMail({
-    from: process.env.CFR,
-    to: process.env.CTO,
-    subject: "kolibee.mx // " + this.data.email,
-    html: `Recibiste un correo de ${this.data.nombre} con el siguiente mensaje: ${this.data.descripcion}`
-  });
-
-  console.log('Message sent: %s', info.messageId);
-
+      const info = await transporter.sendMail({
+        from: process.env.CFR,
+        to: process.env.CTO,
+        subject: "kolibee.mx // " + this.data.email,
+        html: `Recibiste un correo de ${this.data.nombre} con el siguiente mensaje: ${this.data.descripcion}`
+      })
+      //console.log('Message sent: %s', info.messageId)
+      resolve()
+    } else {
+      reject(this.errors)
+		}
+  }) 
 }
 
-module.exports = Mail;
+module.exports = Mail
